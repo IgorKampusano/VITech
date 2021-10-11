1273e4305310e06ad012c792b2cf57d8c3c97a33
IgorKampusano <56488719+IgorKampusano@users.noreply.github.com>
2021-10-11 12:23:13 +0300

    Update task1.php

--- task1.php
+++ task1.php
@@ -4,14 +4,14 @@ $sentence = "Если кто-то оступился, сбился с пути -
 // разбиваем строку на подстроки
 
 // создаем массив для хранения букв
-$firstLetterArr=[];
+$firstLetterArr = [];
 // объявляем функцию
     function firstLetter() {
         global $sentence; // обращаемся к строке глобально
-        $wordsArr=explode(' ', $sentence); // разбиваем строку по символу пробела, результатом функции explode будет массив строк, который запишем в переменную wordsArr
-        for ($i=0; $i<count($wordsArr); $i++) { // объявляем цикл для перебора массива строк
+        $wordsArr = explode(' ', $sentence); // разбиваем строку по символу пробела, результатом функции explode будет массив строк, который запишем в переменную wordsArr
+        for ($i = 0; $i<count($wordsArr); $i++) { // объявляем цикл для перебора массива строк
             if ($wordsArr[$i] != '-') { // проверяем является ли элемент массива словом
-                $firstLetterArr[]=substr($wordsArr[$i], 0, 2); // извлекаем из строки первый символ, результат функции substr добавляем в массив firstLetter 
+                $firstLetterArr[] = substr($wordsArr[$i], 0, 2); // извлекаем из строки первый символ, результат функции substr добавляем в массив firstLetter 
             }
         }
         $firstLetterArr = array_unique($firstLetterArr); // удаляем повторяющиеся элементы из массива
