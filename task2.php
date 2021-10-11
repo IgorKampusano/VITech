9dc3c869d365a6d7b63d99d300ba52b6d978d738
IgorKampusano <56488719+IgorKampusano@users.noreply.github.com>
2021-10-11 14:17:15 +0300

    task2 - task about clock

---
+++ task2.php
@@ -0,0 +1,25 @@
+<?php
+
+$degree=36;
+define('HOUR_DEFAULT', 0.2);
+define('MINUTE_DEFAULT', 12);
+
+
+// Код ниже точно не подойдет
+    // function setTime() {
+    //     if ($degree >= 0 && $degree <= 29) {
+    //         $hour = 12;
+    //     }
+
+    //     else if ($degree >= 30 && $degree <= 59) {
+    //         $hour = 13;
+    //     }
+
+    //     else if ($degree >= 60 && $degree <= 89) {
+    //         $hour = 14;
+    //     }
+
+    //     else if 
+    // }
+
+?>
